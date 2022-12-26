f1de119918ff8f25c5407e97fdccbfb2e7ac1722
Thebks <ichbks@gmail.com>
2022-12-26 18:13:52 +0500

    Loading screen works

--- client/src/page/JoinBattle.jsx
+++ client/src/page/JoinBattle.jsx
@@ -6,6 +6,8 @@ import { CustomButton, PageHOC } from '../components'
 import styles from '../styles'
 
 const JoinBattle = () => {
+
+    const { contract, gameData, setBattleName, setShowAlert, walletAddress } = useGlobalContext();
     const navigate = useNavigate();
     const handleClick = () => {
 
@@ -15,6 +17,18 @@ const JoinBattle = () => {
     return (
         <>
             <h2 className={styles.joinHeadText}> List of Available Battles </h2>
+            <div className={styles.joinContainer}>
+                {gameData.pendingBattles.length
+                    ? gameData.pendingBattles
+                        .filter((battle) => !battle.players.includes(walletAddress))
+                        .map((battle, index) => (
+                            <div className={styles.flexBetween} key={battle.name + index}>
+                                <p className={styles.joinBattleTitle}>{index + 1}: {battle.name}</p>
+                            </div>
+                        ))
+                    : <p className={styles.joinLoading}> Reload to see new battles</p>
+                }
+            </div>
             <p className={styles.infoText} onClick={() => navigate('/create-battle')}>Or Create a new Battle</p>
             <CustomButton
                 title="Join Battle"
