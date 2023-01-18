c0d55328e031f4c75d4bd45bd7ce00c396ec4a51
Thebks <ichbks@gmail.com>
2023-01-18 20:22:12 +0500

    added tiltEffect

--- client/src/components/ActionButton.jsx
+++ client/src/components/ActionButton.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-const ActionButton = () => {
+import styles from '../styles'
+
+
+const ActionButton = ({ imgUrl, handleClick, restStyles }) => {
     return (
-        <div>ActionButton</div>
+        <div className={`${styles.gameMoveBox} ${styles.flexCenter} ${styles.glassEffect} ${restStyles}`}>
+            <img src={imgUrl} alt="action_img" className={styles.gameMoveIcon} />
+        </div>
     )
 }
 
