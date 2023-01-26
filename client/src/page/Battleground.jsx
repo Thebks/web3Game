5a7b8580d870a841a314ecc4003bea77d049ec27
Thebks <ichbks@gmail.com>
2023-01-26 19:54:24 +0500

    battleground

--- client/src/page/Battleground.jsx
+++ client/src/page/Battleground.jsx
@@ -9,6 +9,11 @@ import { useGlobalContext } from '../context';
 
 const Battleground = () => {
     const { setBattleground, setShowAlert, showAlert } = useGlobalContext();
+    const navigate = useNavigate();
+
+    const handleBattleGroundChoice = (ground) => {
+
+    }
 
     return (
         <div className={`${styles.flexCenter} ${styles.battlegroundContainer}`}>
@@ -18,6 +23,21 @@ const Battleground = () => {
                 Choose Your<span className="text-siteViolet"> Battle </span>
                 Ground
             </h1>
+
+            <div className={`${styles.flexCenter} ${styles.battleGroundsWrapper}`}>
+                {battlegrounds.map(ground => (
+                    <div
+                        key={ground.id}
+                        className={`${styles.flexCenter} ${styles.battleGroundCard}`}
+                        onClick={() => handleBattleGroundChoice(ground)}
+                    >
+                        <img src={ground.image} alt="ground" className={styles.battleGroundCardImg} />
+                        <div className="info absolute">
+                            <p className={styles.battleGroundCardText}>{ground.name}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
